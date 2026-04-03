3dcb5b6d57307fc273924755b5bb58468a4f50da
Angelique Miller <am2005@byu.edu>
2026-04-03 15:34:39 -0600

    Phase 6: CRUD functionality added

--- Bookstore.API/backend/Controllers/BookController.cs
+++ Bookstore.API/backend/Controllers/BookController.cs
@@ -27,5 +27,60 @@ namespace Books.API.Controllers
             return something;
         }
 
+
+        // CREATE
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Books.Add(book);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(book);
+        }
+        // UPDATE
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var book = _context.Books.Find(id);
+            return View(book);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Books.Update(book);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(book);
+        }
+        // DELETE
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var book = _context.Books.Find(id);
+            return View(book);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var book = _context.Books.Find(id);
+            _context.Books.Remove(book);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
